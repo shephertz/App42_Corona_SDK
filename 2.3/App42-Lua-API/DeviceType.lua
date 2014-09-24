2b65d8d491fb33f79adc2f78134f4c71476e9572
SamitaMShephertz <samita.mahajan@shephertz.co.in>
2014-09-24 23:29:27 +0530

    2.3 move to 2.x
    Signed-off-by: SamitaMShephertz <samita.mahajan@shephertz.co.in>

--- 2.3/App42-Lua-API/DeviceType.lua
+++
@@ -1,7 +0,0 @@
---author Himanshu Sharma
-DeviceType = {
-  ["ANDROID"] = "ANDROID";
-  ["iOS"] = "iOS";
-  ["WP7"] = "WP7";
-  ["NOKIAX"] = "NokiaX";
-}
--- 2.3/App42-Lua-API/OrderByType.lua
+++
@@ -1,4 +0,0 @@
-OrderByType={
-  ["ASCENDING"] = "ASCENDING";
-  ["DESCENDING"] = "DESCENDING";  
-  }
